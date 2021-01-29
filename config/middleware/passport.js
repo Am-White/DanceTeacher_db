e05997bdf1ff056d7a30a457f808ba1f505c2d5d
littlefire <aybwhite@gmail.com>
2021-01-29 13:51:40 -0800

    first attempt eslint

--- config/middleware/passport.js
+++ config/middleware/passport.js
@@ -1,13 +1,13 @@
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-var db = require("../models");
+var db = require('../models');
 
 // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
 passport.use(new LocalStrategy(
   // Our user will sign in using an email, rather than a "username"
   {
-    usernameField: "email"
+    usernameField: 'email'
   },
   function(email, password, done) {
     // When a user tries to sign in this code runs
@@ -19,13 +19,13 @@ passport.use(new LocalStrategy(
       // If there's no user with the given email
       if (!dbUser) {
         return done(null, false, {
-          message: "Incorrect email."
+          message: 'Incorrect email.'
         });
       }
       // If there is a user with the given email, but the password the user gives us is incorrect
       else if (!dbUser.validPassword(password)) {
         return done(null, false, {
-          message: "Incorrect password."
+          message: 'Incorrect password.'
         });
       }
       // If none of the above, return the user
