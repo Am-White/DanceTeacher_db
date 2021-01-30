b31311cabd51fd9d3ae4157d33cc6875339e8df9
littlefire <aybwhite@gmail.com>
2021-01-29 20:00:31 -0800

    temp fixed code

--- server.js
+++ server.js
@@ -4,47 +4,45 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
+
 require('dotenv').config();
+var session = require('express-session')
 var express = require("express");
 var exphbs = require("express-handlebars");
-<<<<<<< HEAD
 var passport = require('./config/passport')
-
-=======
-// eslint-disable-next-line no-unused-vars
-const path = require("path");
-var bodyParser = require('body-parser')
->>>>>>> parent of 8058ac1... Merge branch 'main' into ame-16
 // console.log(process.env)
-
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
-
 // Requiring our models for syncing
 var db = require("./models");
-
-app.use(express.static(__dirname + "/public"));
-
 // app.use(express.static(path.join(__dirname,"public")));
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-
-
+app.use(express.static(__dirname + "/public"));
+// For Passport
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+// Handlebars
 app.use(express.static('views'));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
+const userAuth = require("./routes/userAuthRoutes.js");
+const userR = require("./routes/userAuthRoutes.js");
 require("./routes/dance-api-routes.js")(app);
 require("./routes/class-api-routes.js")(app);
 require("./routes/instructor-api-routes.js")(app);
-
+app.use(userAuth,userR);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync().then(function() {
@@ -52,5 +50,3 @@ db.sequelize.sync().then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-// { force: true }
